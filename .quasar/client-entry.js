3e61501e35ddcc95a0fa0da3d94df87f0e7277e4
lapin <va.mercier@gmail.com>
2019-01-18 19:40:31 +0100

    save point

---
+++ .quasar/client-entry.js
@@ -0,0 +1,109 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+import 'quasar-framework/dist/quasar.ie.polyfills.js'
+
+
+
+import 'quasar-extras/roboto-font/roboto-font.css'
+
+import 'quasar-extras/material-icons/material-icons.css'
+
+import 'quasar-extras/ionicons/ionicons.css'
+
+import 'quasar-extras/mdi/mdi.css'
+
+import 'quasar-extras/fontawesome/fontawesome.css'
+
+
+
+
+import 'quasar-app-styl'
+
+
+import 'src/css/app.styl'
+
+
+import Vue from 'vue'
+import createApp from './app.js'
+
+
+
+
+import pI18n from 'src/plugins/i18n'
+
+import pAxios from 'src/plugins/axios'
+
+import pVuelidate from 'src/plugins/vuelidate'
+
+
+
+
+
+
+
+
+
+
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
+
+
+
+console.info('[Quasar] Running SPA with MAT theme.')
+
+
+
+const { app, store, router } = createApp()
+
+
+
+
+;[pI18n,pAxios,pVuelidate].forEach(plugin => {
+  plugin({
+    app,
+    router,
+    store,
+    Vue,
+    ssrContext: null
+  })
+})
+
+
+
+
+
+
+
+function getUrlVars() {
+  var vars = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+
+var vars=getUrlVars();
+if (vars["api"]!=undefined)
+{  
+  alert(JSON.stringify(vars))
+  store.state.apiurl=vars["api"].split('#')[0];
+}
+
+
+
+new Vue(app)
+
+
+
+
+
+
---
+++ .quasar/import-quasar.js
@@ -0,0 +1,17 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QInput,QList,QListHeader,QItem,QItemMain,QItemSide,Ripple,Notify,Dialog} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QInput,QList,QListHeader,QItem,QItemMain,QItemSide},directives: {Ripple},plugins: {Notify,Dialog} })
