79d9e6515301af109e0e2dd1af7b9356b91d3218
Pierre Fourniret <pifour.dev@protonmail.com>
2020-11-09 15:31:14 +0100

    ajout globalfunctions.js

--- src/globalfunctions.js
+++ src/globalfunctions.js
@@ -1,18 +1,38 @@
 export function computeTranslatedText(inText, inLocale) {
-    if (inText == undefined)
-      return "";
-    if (!(inText.startsWith("@") && inText.endsWith("@")))
-      return inText;
-    var res = inText.substring(1, inText.length - 1).split(";");
-  
-    var resht = {}
-    res.forEach((x) => {
-      var rec = x.split("=");
-      if (rec.length > 0) {
-        resht[rec[0]] = rec[1];
-      }
-    });
-    if (resht[inLocale] != undefined)
-      return resht[inLocale]
-    return res;
+  if (inText == undefined) return "";
+  if (!(inText.startsWith("@") && inText.endsWith("@"))) return inText;
+  var res = inText.substring(1, inText.length - 1).split(";");
+
+  var resht = {};
+  res.forEach(x => {
+    var rec = x.split("=");
+    if (rec.length > 0) {
+      resht[rec[0]] = rec[1];
+    }
+  });
+  if (resht[inLocale] != undefined) return resht[inLocale];
+  return res;
+}
+
+import Vue from "vue";
+import moment from "moment";
+
+// a retirer
+Vue.filter("formatDate", function(value) {
+  if (value) {
+    return moment(String(value)).format("DD/MM");
   }
+});
+
+Vue.filter("dateShort", function(value) {
+  if (value) {
+    return moment(String(value)).format("DD/MM");
+  }
+});
+
+Vue.filter("dateFrench", function(value) {
+  if (value) {
+    moment.locale("fr");
+    return moment(String(value)).format("DD MMMM YYYY");
+  }
+});
