996d7705568f5a2f9f83220960946dd66044c301
arnaud kaderi <arnay07@gmail.com>
2021-04-21 14:51:06 +0200

    mvp test

---
+++ src/components/custom/data/index.js
@@ -0,0 +1,29 @@
+const orders = [
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+  {
+    orderNumber: 12863,
+    status: 'jhesgfjhd',
+  },
+]
+export default orders
+
+
