50742fe97e3126b4664120fd46605835672e5f62
arnaud kaderi <arnay07@gmail.com>
2021-04-26 20:29:04 +0200

    changes updated

--- src/store/custom/mvpPrepOrders.js
+++ src/store/custom/mvpPrepOrders.js
@@ -1,14 +1,24 @@
 export default {
   namespaced: true,
   state: {
-    orders: []
+    orders: [],
+    productItems: [],
+    currentOrder: {}
   },
   mutations: {
     mutate_allOrders(state, payload) {
       state.orders = payload
+    },
+    mutate_allProducts(state, payload) {
+      state.productItems = payload
+    },
+    mutate_currentOrder(state, payload) {
+      state.currentOrder = payload
     }
   },
   getters: {
-    allOrders: state => state.orders
+    allOrders: state => state.orders,
+    orderProducts: state => order =>
+      state.productItems.filter(product => product.order === order)
   }
 }
