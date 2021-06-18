47f92c0d456c670b696ac3b56947b689433ecfee
arnaud kaderi <arnay07@gmail.com>
2021-06-18 13:44:50 +0200

    save point

--- src/components/custom/mvpPrepOrders/OrderItem.vue
+++ src/components/custom/mvpPrepOrders/OrderItem.vue
@@ -1,78 +1,78 @@
 <template>
-  <transition
+  <!-- <transition
     enter-active-class="animated fadeIn"
     leave-active-class="animated fadeOut"
     duration="200"
+  > -->
+  <q-item
+    clickable
+    :class="[`bg-${bgColor}-2`, 'item']"
+    v-if="!prepa"
+    @click="success(product)"
+    dense
   >
-    <q-item
-      clickable
-      :class="[`bg-${bgColor}-2`, 'item']"
-      v-if="!prepa"
-      @click="success(product)"
-      dense
+    <q-item-section
+      v-if="product._source.smallImage !== undefined"
+      style="max-width: 60px"
     >
-      <q-item-section
-        v-if="product._source.smallImage !== undefined"
-        style="max-width: 60px"
-      >
-        <img
-          :src="product._source.smallImage"
-          style="width: 50px; height: 50px"
-        />
-      </q-item-section>
-      <q-item-section v-else style="max-width: 60px; ">
-        <img
-          src="https://via.placeholder.com/50"
-          style="width: 50px; height: 50px"
-        />
-      </q-item-section>
-      <div class="flex row col-xs-10">
-        <div class="row col-xs-12 items-center">
-          <div
-            style="font-weight: bold; font-size: 15px; "
-            class="row col-xs-10 justify-start "
-          >
-            {{ product._source.name }}
-          </div>
-          <div class="row col-xs-2 justify-end">
-            <q-badge
-              :class="[`${isFrais ? 'frais' : 'sec'}`, 'justify-center']"
-              style="max-width: 40px"
-            >
-              {{ this.isFrais ? 'Frais' : 'Sec' }}
-            </q-badge>
-          </div>
+      <img
+        :src="product._source.smallImage"
+        style="width: 50px; height: 50px"
+      />
+    </q-item-section>
+    <q-item-section v-else style="max-width: 60px; ">
+      <img
+        src="https://via.placeholder.com/50"
+        style="width: 50px; height: 50px"
+      />
+    </q-item-section>
+    <div class="flex row col-xs-10">
+      <div class="row col-xs-12 items-center">
+        <div
+          style="font-weight: bold; font-size: 15px; "
+          class="row col-xs-10 justify-start "
+        >
+          {{ product._source.name }}
         </div>
-        <div class="row full-width items-center">
-          <q-icon name="location_on" />
-          <div class="row col-xs-6 col-md-9 justify-start">
-            LOC {{ product._source.loc }} | {{ product._source.clean_vendor }}
-          </div>
-          <div class="row col-xs-5 col-md-2 justify-end">
-            <q-btn
-              @click="remb(product)"
-              icon="close"
-              color="red-4"
-              style="color: white; max-width: 40px"
-              size="15px"
-              unelevated
-              rounded
-            ></q-btn>
-            &nbsp; &nbsp;
-            <q-btn
-              @click="manq(product)"
-              icon="hourglass_bottom"
-              color="orange-4"
-              style="color: white; max-width: 40px; margin-left: 20px"
-              size="15px"
-              unelevated
-              rounded
-            ></q-btn>
-          </div>
+        <div class="row col-xs-2 justify-end">
+          <q-badge
+            :class="[`${isFrais ? 'frais' : 'sec'}`, 'justify-center']"
+            style="max-width: 40px"
+          >
+            {{ this.isFrais ? 'Frais' : 'Sec' }}
+          </q-badge>
         </div>
       </div>
-    </q-item>
-  </transition>
+      <div class="row full-width items-center">
+        <q-icon name="location_on" />
+        <div class="row col-xs-6 col-md-9 justify-start">
+          LOC {{ product._source.loc }} | {{ product._source.clean_vendor }}
+        </div>
+        <div class="row col-xs-5 col-md-2 justify-end">
+          <q-btn
+            @click="remb(product)"
+            icon="close"
+            color="red-4"
+            style="color: white; max-width: 40px"
+            size="15px"
+            unelevated
+            rounded
+          ></q-btn>
+          &nbsp; &nbsp;
+          <q-btn
+            @click="manq(product)"
+            icon="hourglass_bottom"
+            color="orange-4"
+            style="color: white; max-width: 40px; margin-left: 20px"
+            size="15px"
+            unelevated
+            rounded
+          ></q-btn>
+        </div>
+      </div>
+    </div>
+  </q-item>
+  <!-- </transition> -->
 </template>
 
 <script>
